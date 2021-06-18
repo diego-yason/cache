e7bef46bf4e924a01c03330354ce74fbbb71517b
KingMarine-GH <61825575+KingMarine-GH@users.noreply.github.com>
2021-06-18 19:03:56 +0800

    added more tests

--- test/set.test.js
+++ test/set.test.js
@@ -1,10 +1,11 @@
 const Cache = require("../index.js");
 
- describe("Tests Cache class", () => {
+describe("Tests Cache class", () => {
 
     let cache = new Cache();
 
     beforeEach(() => {
+        jest.useRealTimers();
         cache = new Cache();
     })
 
@@ -17,9 +18,9 @@ const Cache = require("../index.js");
         cache.add("test", "hi", 1);
         cache.add("notest", "lies.", "hello");
 
-        expect(cache.get("test")).toBe("hello");
-        expect(cache.get("test", 1)).toBe("hi");
-        expect(cache.get("wrong_id")).toBeUndefined();
+        expect(cache.get("test").data).toBe("hello");
+        expect(cache.get("test", 1).data).toBe("hi");
+        expect(cache.get("wrong_id").data).toBeUndefined();
     });
 
     it("can block invalid adds", () => {
@@ -33,7 +34,45 @@ const Cache = require("../index.js");
         expect(cache.add("test", "def").code).toBe(1);
     })
     
-    it.todo("can delete values at will");
+    it("can delete values at will", () => {
+        cache.add("test", "zdc");
 
-    it.todo("can have ttl on values");
+        expect(cache.get("test").code).toBe(0);
+
+        cache.remove("test");
+
+        expect(cache.get("test").data).toBeUndefined();
+    });
+
+    it("can have ttl on values", () => {
+        cache = new Cache(100000);
+
+        jest.useFakeTimers();
+        
+        cache.add("test", 0);
+        
+        expect(cache.get("test")).toBeDefined();
+
+        jest.advanceTimersByTime(100000);
+
+        expect(cache.get("test").data).toBeUndefined()
+
+        cache = new Cache(100000, true);
+
+        cache.add("test", 1);
+
+        expect(cache.get("test")).toBeDefined();
+
+        jest.advanceTimersByTime(99999);
+
+        expect(cache.get("test")).toBeDefined();
+
+        jest.advanceTimersByTime(1000);
+
+        expect(cache.get("test")).toBeDefined();
+
+        jest.advanceTimersByTime(100000);
+
+        expect(cache.get("test").data).toBeUndefined();
+    });
 });
